a39c771b72cd9a28018b2405877347ae268f0503
faizan ul hasnain59 <faizanulhasnain188@gmail.com>
2023-07-13 21:48:52 -0700

    five_comment

--- server.mjs
+++ server.mjs
@@ -4,12 +4,12 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   console.log("~file:server.mjs:7~app.get ~ req:" + req);
-  res.send('Hello World! by Faizan');
+  res.send({message: "Hello World! by Faizan"});
 })
 
 app.get('/name:/myName', (req, res) => {
   const name = req.params.myName;
-  res.send('Hello World! by ${name}');
+  res.send({message: "Hello world! by ${name}"});
 })
 
 app.listen(PORT, () => {
